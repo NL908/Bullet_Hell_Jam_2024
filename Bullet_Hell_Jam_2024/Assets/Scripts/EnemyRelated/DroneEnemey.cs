45ea5b25039418a154b9b93d00e6276517ada40a
Yunt Flare <yunt_flare@outlook.com>
2024-05-14 00:30:18 -0400

    create outline for enemy prefab and scripts

---
+++ Bullet_Hell_Jam_2024/Assets/Scripts/EnemyRelated/DroneEnemey.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DroneEnemey : Enemy
+{
+
+    public static void Intiazlize()
+    {
+
+    }
+
+    // Handle events on hit
+    protected void onHit()
+    {
+
+    }
+
+    /* Engine Method */
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+                
+    }
+    private void OnBecameInvisible()
+    {
+        
+    }
+
+    private void Start()
+    {
+        
+    }
+}
