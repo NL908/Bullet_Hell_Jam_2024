0ec5b1b40b27344f4a9928064d652fa0da1f6745
Nanqi-Liu <fafalnq@gmail.com>
2024-05-18 14:51:06 -0400

    Added Timer UI update
    Know issue: Timer UI needs to be formatted with fixed decimal place

--- Bullet_Hell_Jam_2024/Assets/Scripts/GameMaster.cs
+++ Bullet_Hell_Jam_2024/Assets/Scripts/GameMaster.cs
@@ -7,6 +7,8 @@ public class GameMaster : MonoBehaviour
 {
     public static GameMaster instance;
 
+    public bool isGameStarted = false;
+
     public Vector2 arenaSize;
 
     private float score;
@@ -24,6 +26,24 @@ public class GameMaster : MonoBehaviour
         }
     }
 
+    // The maximum time for the game in seconds
+    [SerializeField]
+    public int maxTime = 120;
+    private float currTimer;
+    public float CurrTimer
+    {
+        get { return currTimer; }
+        set
+        {
+            currTimer = value;
+            try
+            {
+                CanvasScript.instance.UpdateTime(currTimer);
+            }
+            catch (Exception e) { Debug.LogError("Timer UI update error with: " + e.Message); }
+        }
+    }
+
     private void Awake()
     {
         instance = this;
@@ -32,16 +52,35 @@ public class GameMaster : MonoBehaviour
     private void Start()
     {
         Score = 0;
+        CurrTimer = maxTime;
+    }
+
+    private void LateUpdate()
+    {
+        // Reduce & update timer here
+        if (isGameStarted)
+        {
+            CurrTimer -= Time.deltaTime;
+
+            if (CurrTimer <= 0)
+            {
+                Debug.Log("Time is over!");
+                GameOver();
+            }
+        }
     }
 
     public void StartGame()
     {
+        isGameStarted = true;
         EnemyGenerationManager.instance.isActive = true;
         Score = 0;
+        CurrTimer = maxTime;
     }
 
     public void GameOver()
     {
         Debug.Log("Game over");
+        isGameStarted = false;
     }
 }
