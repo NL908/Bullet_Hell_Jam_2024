db7848400a24a7448b84a1976d0b0b5274bd74a2
Nanqi-Liu <fafalnq@gmail.com>
2024-05-19 12:37:24 -0400

    Adjusted homing lock on

--- Bullet_Hell_Jam_2024/Assets/Scripts/ProjectileRelated/HomingProjectile.cs
+++ Bullet_Hell_Jam_2024/Assets/Scripts/ProjectileRelated/HomingProjectile.cs
@@ -11,16 +11,24 @@ public class HomingProjectile : Projectile
     private float _enemyDirectionRadius;
     [SerializeField]
     private float _turnRate = 360f;
+    private float _maxTurnRate = 3600f;
+
+    private Transform targetEnemyTransform;
 
     protected override void Start()
     {
         base.Start();
         _turnRate *= Mathf.Deg2Rad;
+        _maxTurnRate *= Mathf.Deg2Rad;
     }
 
     private void FixedUpdate()
     {
-        FindEnemy();
+        if(targetEnemyTransform == null)
+        {
+            FindEnemy();
+        }
+        RotateHoming();
         ForwardMove();
     }
 
@@ -34,10 +42,19 @@ public class HomingProjectile : Projectile
         RaycastHit2D hit = Physics2D.CircleCast(transform.position, _enemyDirectionRadius, Vector2.up, 0, _enemyLayer, 0);
         if (hit)
         {
-            Vector3 targetDirection = hit.transform.position - transform.position;
-            
+            targetEnemyTransform = hit.transform;
+        }
+    }
+
+    private void RotateHoming()
+    {
+        if (targetEnemyTransform)
+        {
+            Vector3 targetDirection = targetEnemyTransform.position - transform.position;
+
             Vector3 aimDirection = Vector3.RotateTowards(transform.up, targetDirection, _turnRate * Time.deltaTime, 0.0f);
             transform.rotation = Quaternion.LookRotation(Vector3.forward, aimDirection);
+            _turnRate = Mathf.Min(_turnRate + Mathf.PI * Time.deltaTime, _maxTurnRate);
         }
     }
 }
